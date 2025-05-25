47487bbdd0b9080721feac8ac4951deb6d5d8cb3
Kazuya Nomura <knomura@c2r1s1.42tokyo.jp>
2025-05-25 19:31:49 +0900

    first

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/23 01:15:57 by knomura           #+#    #+#             */
+/*   Updated: 2025/05/23 06:34:44 by knomura          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <limits.h>
+
+char	*get_next_line(int fd);
+
+#endif
