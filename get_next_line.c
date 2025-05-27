d52c0fd9191e7c921c8ab32644f0ef1cabe45a04
Kazuya Nomura <knomura@c4r3s3.42tokyo.jp>
2025-05-28 06:15:25 +0900

    5/28

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,38 @@
 /*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 01:16:00 by knomura           #+#    #+#             */
-/*   Updated: 2025/05/25 16:25:39 by knomura          ###   ########.fr       */
+/*   Updated: 2025/05/28 04:40:16 by knomura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+void	*free_stash(char **stash)
+{
+	if (stash && *stash)
+	{
+		free(*stash);
+		*stash = NULL;
+	}
+	return (NULL);
+}
+char	*read_and_stash(int fd, char )
+{
+
+	return (0);
+}
+
 char	*get_next_line(int fd)
 {
-	
-	return (NULL);
+	static char		*stash;
+	char			*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	stash = read_and_stash(fd, stash);
+	if (!stash)
+		return (NULL);
+	line = extract_line(stash);
+	stash = clean_stash(stash);
+	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,25 @@
 /*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 01:15:57 by knomura           #+#    #+#             */
-/*   Updated: 2025/05/23 06:34:44 by knomura          ###   ########.fr       */
+/*   Updated: 2025/05/28 04:03:28 by knomura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
 # include <stdlib.h>
 # include <limits.h>
 
 char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
