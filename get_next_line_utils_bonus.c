b19ee93cf3f4cacd3276c4cf511130464d06f13a
Kazuya Nomura <knomura@c2r3s9.42tokyo.jp>
2025-06-02 17:27:34 +0900

    finito 2

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 01:16:02 by knomura           #+#    #+#             */
-/*   Updated: 2025/06/02 14:14:23 by knomura          ###   ########.fr       */
+/*   Updated: 2025/06/02 17:15:28 by knomura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,25 +84,3 @@ char	*ft_strdup(const char *s1)
 	dest[i] = '\0';
 	return (dest);
 }
-
-// char	*ft_substr(char const *s, unsigned int start, size_t len)
-// {
-// 	char	*dest;
-// 	size_t	i;
-
-// 	if (!s || start >= ft_strlen(s))
-// 		return (ft_strdup(""));
-// 	if (len > ft_strlen(s) - start)
-// 		len = ft_strlen(s) - start;
-// 	dest = malloc(len + 1);
-// 	if (!dest)
-// 		return (NULL);
-// 	i = 0;
-// 	while (i < len && s[start + i])
-// 	{
-// 		dest[i] = s[start + i];
-// 		i++;
-// 	}
-// 	dest[i] = '\0';
-// 	return (dest);
-// }
